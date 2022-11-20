1c8a16cf7ba6a570dbb627bef0eee7747f52bbce
Johannes Normann Jensen <johannesnormannjensen@gmail.com>
2022-11-20 22:24:44 +0100

    chore: add emulator auth conf

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,20 @@
+import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+initializeApp({
+  credential: applicationDefault()
+});
+
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  await getAuth().createUser({uid: 'test', email: 'hej@hej.dk'});
+  await getFirestore().collection('hej').add({ ehuheue: 'hahah' });
+
+  response.send("Hello from Firebase!");
+});
