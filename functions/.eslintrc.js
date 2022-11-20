39db720466ae3598acc67d908ee7b6c34a413a5f
Johannes Normann Jensen <johannesnormannjensen@gmail.com>
2022-11-20 22:42:46 +0100

    ci: initial test setup

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: {
-    es6: true,
+    es2020: true,
     node: true,
   },
   extends: [
@@ -18,6 +18,7 @@ module.exports = {
     sourceType: "module",
   },
   ignorePatterns: [
+    "jest.config.js",
     "/lib/**/*", // Ignore built files.
   ],
   plugins: [
