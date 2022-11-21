e73b105e85d34e2912e1d9e98cdb02a5c734d38e
Johannes Normann Jensen <johannesnormannjensen@gmail.com>
2022-11-21 19:55:28 +0100

    revert

--- functions/src/auth/on-create.ts
+++ functions/src/auth/on-create.ts
@@ -3,5 +3,5 @@ import { UserRecord } from 'firebase-admin/auth';
 import { auth, EventContext } from 'firebase-functions';
 
 export const authOnCreate = auth.user().onCreate(async (user: UserRecord, res: EventContext<Record<string, string>>) => {
-  await firestore().collection('user').doc(user.uid).create({dis});
+  await firestore().collection('user').doc(user.uid).create(user.toJSON());
 });
